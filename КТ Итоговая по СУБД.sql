2011175a0a75412d4af65ebd8ff6a00b429bdbd7
NikitaBestfator <nbestfator@gmail.com>
2026-05-06 13:39:07 +0300

    Update КТ Итоговая по СУБД.sql

--- КТ Итоговая по СУБД.sql
+++ КТ Итоговая по СУБД.sql
@@ -48,3 +48,46 @@ CREATE INDEX idx_loans_due_date ON loans(due_date);
 
 -- 4 аномалия
 
+SHOW TRIGGERS FROM exam_bestfator WHERE `Table` = 'loans';
+
+-- как исправить
+
+-- Триггер на выдачу книги (INSERT в loans)
+DELIMITER $$
+CREATE TRIGGER trg_loans_after_insert
+AFTER INSERT ON loans
+FOR EACH ROW
+BEGIN
+    UPDATE books
+    SET available_copies = available_copies - 1
+    WHERE id = NEW.book_id AND available_copies > 0;
+END$$
+
+-- Триггер на возврат книги (UPDATE return_date в loans)
+CREATE TRIGGER trg_loans_after_update
+AFTER UPDATE ON loans
+FOR EACH ROW
+BEGIN
+    IF OLD.return_date IS NULL AND NEW.return_date IS NOT NULL THEN
+        UPDATE books
+        SET available_copies = available_copies + 1
+        WHERE id = NEW.book_id;
+    END IF;
+END$$
+DELIMITER ;
+
+-- 5 аномалия
+
+SHOW FULL TABLES IN exam_bestfator WHERE Tables_in_exam_bestfator = 'active_loans';
+
+-- как исправить
+
+-- Создать представление
+CREATE VIEW active_loans_view AS
+SELECT * FROM loans WHERE return_date IS NULL;
+
+-- Удалить старую таблицу (если не нужна)
+DROP TABLE active_loans;
+
+-- 6 аномалия
+
