447402038169c51b20a01ff56a2e4a5654731cc2
NikitaBestfator <nbestfator@gmail.com>
2026-05-06 12:47:08 +0300

    Update and rename КТ Итоговая по СУБД to КТ Итоговая по СУБД.sql

--- КТ Итоговая по СУБД.sql
+++ КТ Итоговая по СУБД.sql
@@ -24,3 +24,27 @@ ADD CONSTRAINT fk_loans_reader_id
 FOREIGN KEY (reader_id) REFERENCES readers(id)
 ON DELETE SET NULL ON UPDATE CASCADE;
 
+-- 2 аномалия
+
+SELECT id, book_id, reader_id, loan_date, due_date
+FROM loans
+WHERE due_date < loan_date;
+
+-- как исправить
+
+UPDATE loans
+SET due_date = loan_date,
+    loan_date = due_date
+WHERE due_date < loan_date;
+
+-- 3 аномалия
+
+SHOW INDEX FROM loans WHERE Column_name IN ('loan_date', 'due_date');
+
+-- как исправить
+
+CREATE INDEX idx_loans_loan_date ON loans(loan_date);
+CREATE INDEX idx_loans_due_date ON loans(due_date);
+
+-- 4 аномалия
+
